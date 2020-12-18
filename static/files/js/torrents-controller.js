6e320e08f5e0bfcc5099ff297eae5f305a1e95c0
Anab Khan <akanabkhan@gmail.com>
2020-12-18 17:45:21 +0000

    fix

--- static/files/js/torrents-controller.js
+++ static/files/js/torrents-controller.js
@@ -49,7 +49,7 @@ app.controller("TorrentsController", function($scope, $rootScope, api, $sce) {
 
   $scope.onVideoElementLoaded = function() {
     var options = {};
-      var player = videojs('my-player', options, function onPlayerReady() {
+      var player = videojs('video-js vjs-theme-city', options, function onPlayerReady() {
         videojs.log('Your player is ready!');
 
         // In this context, `this` is the player that was created by Video.js.
