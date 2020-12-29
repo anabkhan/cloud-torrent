877ad3f065a80d72272f4bac5d95e800d0306160
Anab Khan <akanabkhan@gmail.com>
2020-12-29 17:03:50 +0000

    removed logs

--- server/server_search.go
+++ server/server_search.go
@@ -56,8 +56,6 @@ func (s *Server) fetchSearchConfig() error {
 	s.state.Push()
 	// currentConfig = newConfig
 	log.Printf("Loaded new search providers")
-  scr, _ := json.MarshalIndent(s.state.SearchProviders, "", "\t");
-  log.Print(scr)
 	return nil
 }
 
